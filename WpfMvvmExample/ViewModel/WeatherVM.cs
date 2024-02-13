98fe507d88546a81ccfb72fe4b6d8df6481cef7f
Donald Sincennes <donald.sincennes@leonardocompany.ca>
2024-02-13 11:50:29 -0500

    VM changes

---
+++ WpfMvvmExample/ViewModel/WeatherVM.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfMvvmExample.Model;
+
+namespace WpfMvvmExample.ViewModel
+{
+    public class WeatherVM : INotifyPropertyChanged
+    {
+        private string query;
+
+        public string Query
+        {
+            get { return query; }
+            set 
+            { 
+                query = value;
+                onPropertyChanged(nameof(Query));
+            }
+        }
+
+        private CurrentConditions currentConditions;
+
+        public CurrentConditions CurrentConditions
+        {
+            get { return currentConditions; }
+            set 
+            { 
+                currentConditions = value;
+                onPropertyChanged(nameof(CurrentConditions));
+            }
+        }
+
+        private City selectedCity;
+
+        public City SelectedCity
+        {
+            get { return selectedCity; }
+            set 
+            { 
+                selectedCity = value; 
+                onPropertyChanged($"{nameof(SelectedCity)}");
+            }
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void onPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
